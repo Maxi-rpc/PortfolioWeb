7ee2ee901a7794b483e6504d94ce17468ec2ddc2
Maximiliano <maxirpc2607@gmail.com>
2021-05-18 18:29:54 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
-let test = "Desde el DOM";
-
+let test = "Hola Mundo desde el DOM";
 console.log(test);
 
 /* Fecha del footer */
 let tiempo = document.getElementById("tiempo");
 let date = new Date().getFullYear();
 tiempo.innerHTML = date;
+
+/* Objeto Project */
+class Project {
+  constructor(lugar, tipo, titulo, tecnologia, descripcion, link) {
+    this.lugar = lugar;
+    this.tipo = tipo;
+    this.titulo = titulo;
+    this.tecnologia = tecnologia;
+    this.descripcion = descripcion;
+    this.link = link;
+  }
+}
+
+let project = new Project(
+  "UTN - FRGP - Técnicatura en Programación",
+  "Software",
+  "- S.A.S.A",
+  "C - C++",
+  "Sistema de Administración de Stock Almacén, aplicación en consola donde se realizan Altas Bajas y Modificación de inventario",
+  "www.google.com"
+);
+
+console.log(project);
