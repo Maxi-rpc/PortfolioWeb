83f250bf3ed05da4ba092cb361ca7e9dcf7d0e0b
Maximiliano <maxirpc2607@gmail.com>
2021-05-22 18:00:25 -0300

    Update
    Se adjunta nuevo BIO, fav ico, icono del nav, se cambian Portfolio de ver codigo a github icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,27 @@ tiempo.innerHTML = date;
 
 /* Objeto Project */
 class Project {
-  constructor(lugar, tipo, titulo, tecnologia, descripcion, link) {
-    this.lugar = lugar;
-    this.tipo = tipo;
-    this.titulo = titulo;
-    this.tecnologia = tecnologia;
-    this.descripcion = descripcion;
-    this.link = link;
-  }
+	constructor(lugar, tipo, titulo, tecnologia, descripcion, link) {
+		this.lugar = lugar;
+		this.tipo = tipo;
+		this.titulo = titulo;
+		this.tecnologia = tecnologia;
+		this.descripcion = descripcion;
+		this.link = link;
+	}
 }
 
 let project = new Project(
-  "UTN - FRGP - Técnicatura en Programación",
-  "Software",
-  "- S.A.S.A",
-  "C - C++",
-  "Sistema de Administración de Stock Almacén, aplicación en consola donde se realizan Altas Bajas y Modificación de inventario",
-  "www.google.com"
+	"UTN - FRGP - Técnicatura en Programación",
+	"Software",
+	"- S.A.S.A",
+	"C - C++",
+	"Sistema de Administración de Stock Almacén, aplicación en consola donde se realizan Altas Bajas y Modificación de inventario",
+	"www.google.com"
 );
 
 console.log(project);
+
+let tipeo = new Typed(".tipeo", {
+	string: ["Maximiliano", "Desarrollador", "Programador", "Dev"],
+});
