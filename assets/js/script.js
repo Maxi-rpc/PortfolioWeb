7465b94e5afc6793bae0e9e1a40ca5c6c7162201
Maximiliano <maxirpc2607@gmail.com>
2021-05-22 18:47:35 -0300

    Fix media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,3 @@ console.log(test);
 let tiempo = document.getElementById("tiempo");
 let date = new Date().getFullYear();
 tiempo.innerHTML = date;
-
-/* Objeto Project */
-class Project {
-	constructor(lugar, tipo, titulo, tecnologia, descripcion, link) {
-		this.lugar = lugar;
-		this.tipo = tipo;
-		this.titulo = titulo;
-		this.tecnologia = tecnologia;
-		this.descripcion = descripcion;
-		this.link = link;
-	}
-}
-
-let project = new Project(
-	"UTN - FRGP - Técnicatura en Programación",
-	"Software",
-	"- S.A.S.A",
-	"C - C++",
-	"Sistema de Administración de Stock Almacén, aplicación en consola donde se realizan Altas Bajas y Modificación de inventario",
-	"www.google.com"
-);
-
-console.log(project);
-
-let tipeo = new Typed(".tipeo", {
-	string: ["Maximiliano", "Desarrollador", "Programador", "Dev"],
-});
